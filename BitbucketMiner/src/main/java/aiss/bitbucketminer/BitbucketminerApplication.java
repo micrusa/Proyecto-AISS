d978b0dffcb573c3885d2b92d02981d4bed31165
Miguel Cruces <micrusa@hotmail.es>
2025-04-11 13:31:28 +0200

    Proyectos base GithubMiner y BitbucketMiner

---
+++ BitbucketMiner/src/main/java/aiss/bitbucketminer/BitbucketminerApplication.java
@@ -0,0 +1,13 @@
+package aiss.bitbucketminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbucketminerApplication.class, args);
+	}
+
+}
