8328fbd232b6b0de3144313dd2c967bc88068749
Miguel Cruces <micrusa@hotmail.es>
2025-04-11 13:59:05 +0200

    GitMiner: Corregir controlador de project

--- GitMiner/src/main/java/aiss/gitminer/controller/ProjectController.java
+++ GitMiner/src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -1,26 +1,29 @@
 package aiss.gitminer.controller;
 
 import aiss.gitminer.model.Project;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/gitminer/projects")
 public class ProjectController {
     @GetMapping
-    public String getAll() { // TODO
+    public ResponseEntity<List<Project>> getAll() { // TODO
         throw new UnsupportedOperationException("Por implementar");
     }
 
     @GetMapping("/{id}")
-    public String getById(
+    public ResponseEntity<Project> getById(
             @PathVariable(name = "id") String id
     ) { // TODO
         throw new UnsupportedOperationException("Por implementar");
     }
 
     @PostMapping
-    public String create(
+    public ResponseEntity<Project> create(
             @RequestBody @NonNull Project project
     ) { // TODO
         throw new UnsupportedOperationException("Por implementar");
