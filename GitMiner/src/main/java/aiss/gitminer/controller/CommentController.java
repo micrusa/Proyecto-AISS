f5b645ca8e04d71d2b01c51bd7d551c3a54891a4
Miguel Cruces <micrusa@hotmail.es>
2025-04-26 17:47:19 +0200

    GitMiner: Comments

--- GitMiner/src/main/java/aiss/gitminer/controller/CommentController.java
+++ GitMiner/src/main/java/aiss/gitminer/controller/CommentController.java
@@ -1,6 +1,9 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.exception.CommentNotFoundException;
 import aiss.gitminer.model.Comment;
+import aiss.gitminer.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,19 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/gitminer/comments")
 public class CommentController {
+    @Autowired
+    private CommentRepository commentRepository;
+
     @GetMapping
-    public ResponseEntity<List<Comment>> getAll() { // TODO
-        throw new UnsupportedOperationException("Por implementar");
+    public ResponseEntity<List<Comment>> getAll() {
+        List<Comment> comments = commentRepository.findAll();
+        return ResponseEntity.ok(comments);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Comment> getById(
             @PathVariable(name = "id") String id
-    ) { // TODO
-        throw new UnsupportedOperationException("Por implementar");
+    ) throws CommentNotFoundException {
+        Optional<Comment> comment = commentRepository.findById(id);
+        Comment existingComment = comment.orElseThrow(CommentNotFoundException::new);
+        return ResponseEntity.ok(existingComment);
     }
 }
